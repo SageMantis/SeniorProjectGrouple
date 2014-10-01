cf2208cb7cbc70eb14e733499994c601c0024419
Mobliz640 <Mobliz640@gmail.com>
2014-09-30 21:55:20 -0400

    Added API files
    php for mysql connection, getlogin, and registeraccount

---
+++ android_connect/get_login.php
@@ -0,0 +1,29 @@
+<?php
+
+	include_once('/../includes/db_connect.inc.php');
+
+	$stmt = $mysqli->prepare("SELECT COUNT(*) FROM user WHERE email = ? AND password = ?");
+	$stmt->bind_param('ss', $_GET['email'], $_GET['password']);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$row = $result->fetch_row();
+
+	header('Content-type: application/json');
+	if($row[0] == 0)
+	{
+		$response["success"] = $row[0];
+		$response["message"] = "Login or password incorrect!";
+		echo json_encode ( $response );
+	}
+	else
+	{
+		$response["success"] = $row[0];
+		$response["message"] = "Successful Login!";
+		echo json_encode ( $response );
+	}	
+
+	$mysqli->close();
+	
+	
+
+?>
