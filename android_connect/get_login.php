b6390cdd19cb6c895673fdabd433736be03aff6e
Mobliz640 <Mobliz640@gmail.com>
2014-10-18 20:47:08 -0400

    Blowfish Encryption for User accounts!
    I have updated registeraccount and getlogin php files to use PHP 5.5's
    super ultra effective Bcrypt algorithm.  All passwords will now be
    stored in 60 char hashes in the database and are also virtually immune
    to rainbow table lookups.  No changes need to be made on the client
    side, it's all ready to go!
    
    Note: All user accounts with plain text passwords have been removed from
    the user table.  please create new accounts for testing.  It is safe to
    use a real password if you want because no one (including ourselves) can
    view the password anymore! Hooray!

--- android_connect/get_login.php
+++ android_connect/get_login.php
@@ -2,24 +2,35 @@
 
 	include_once('/../includes/db_connect.inc.php');
 
-	$stmt = $mysqli->prepare("SELECT COUNT(*) FROM users WHERE email = ? AND password = ?");
-	$stmt->bind_param('ss', $_GET['email'], $_GET['password']);
+	$stmt = $mysqli->prepare("SELECT password FROM users WHERE email = ?");
+	$stmt->bind_param('s', $_GET['email']);
 	$stmt->execute();
 	$result = $stmt->get_result();
 	$row = $result->fetch_row();
-
+	
 	header('Content-type: application/json');
-	if($row[0] == 0)
+	if(mysqli_num_rows($result) < 1)
 	{
-		$response["success"] = $row[0];
-		$response["message"] = "Login or password incorrect!";
+		$response["success"] = 0;
+		$response["message"] = "That email address is not registered!";
 		echo json_encode ( $response );
 	}
 	else
 	{
-		$response["success"] = $row[0];
-		$response["message"] = "Successful Login!";
-		echo json_encode ( $response );
+		
+		if(password_verify($_GET['password'], $row[0]))
+		{
+			$response["success"] = 1;
+			$response["message"] = "Successful Login!";
+			echo json_encode ( $response );
+		}
+		else
+		{
+			$response["success"] = 0;
+			$response["message"] = "Incorrect username or password!";
+			echo json_encode ( $response );
+		}
+		
 	}	
 
 	$mysqli->close();
