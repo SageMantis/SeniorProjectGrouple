11192de7f7893cfc6f5529a2e2ba88a68ab3923b
Brett Mierzejewski <brett@brettstet.com>
2014-12-06 16:22:30 -0500

    Messing with Colors / Layouts
    Feel free to give me some feedback, working on incorporating the sky
    blue to make category colors a bit different without having the colors
    clash.

--- client/Grouple/src/cs460/grouple/grouple/GroupProfileActivity.java
+++ client/Grouple/src/cs460/grouple/grouple/GroupProfileActivity.java
@@ -1,27 +1,75 @@
 package cs460.grouple.grouple;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import cs460.grouple.grouple.R;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class GroupProfileActivity extends ActionBarActivity {
+public class GroupProfileActivity extends ActionBarActivity 
+{
 
+	private ImageView iv;
+	private Bitmap bmp;
 	BroadcastReceiver broadcastReceiver;
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_group_profile);
 
+		/*Action bar*/
+		ActionBar ab = getSupportActionBar();
+		ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		ab.setCustomView(R.layout.actionbar);
+		ab.setDisplayHomeAsUpEnabled(true);
+		TextView actionbarTitle = (TextView) findViewById(R.id.actionbarTitleTextView);
+		Global global = ((Global) getApplicationContext());
+		Bundle extras = getIntent().getExtras();
+		String gname = extras.getString("gname");
+		actionbarTitle.setText(gname);
+
+		initGroupTable();
 		initKillswitchListener();
 	}
 
+	public void initGroupTable(){
+		final LayoutInflater inflater = getLayoutInflater();
+	}
+	
 	@Override
 	protected void onDestroy()
 	{
@@ -29,16 +77,25 @@ public class GroupProfileActivity extends ActionBarActivity {
 		unregisterReceiver(broadcastReceiver);
 		super.onDestroy();
 	}
-	
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.navigation_actions, menu);
+
+		// Set up the image view
+		if (iv == null)
+		{
+			iv = (ImageView) findViewById(R.id.profilePhoto);
+		}
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
@@ -51,6 +108,8 @@ public class GroupProfileActivity extends ActionBarActivity {
 			global.setDeclineEmail("");
 			Intent login = new Intent(this, LoginActivity.class);
 			startActivity(login);
+			bmp = null;
+			iv = null;
 			Intent intent = new Intent("CLOSE_ALL");
 			this.sendBroadcast(intent);
 			return true;
@@ -62,28 +121,163 @@ public class GroupProfileActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			startHomeActivity(null);
+		}
+		return false;
+	}
+
+
+	/* Start activity functions for going back to home and logging out */
+	public void startHomeActivity(View view)
+	{
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
+		bmp = null;
+		iv = null;
+		finish();
+	}
+
+	public void startEditProfileActivity(View view)
+	{
+		Intent intent = new Intent(this, EditProfileActivity.class);
+		startActivity(intent);
+		bmp = null;
+		iv = null;
+	}
+
+	/*
+	 * Get profile executes get_profile.php.
+	 */
+	private class getProfileTask extends AsyncTask<String, Void, String>
+	{
+
+		protected String doInBackground(String... urls)
+		{
+
+			return readJSONFeed(urls[0]);
+		}
+
+		public String readJSONFeed(String URL)
+		{
+
+			StringBuilder stringBuilder = new StringBuilder();
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(URL);
+			try
+			{
+				Global global = ((Global) getApplicationContext());
+				String email = global.getCurrentUser();
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+				nameValuePairs.add(new BasicNameValuePair("email", email));
+				httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+				HttpResponse response = httpClient.execute(httpPost);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200)
+				{
+					HttpEntity entity = response.getEntity();
+					InputStream inputStream = entity.getContent();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(inputStream));
+					String line;
+					while ((line = reader.readLine()) != null)
+					{
+						stringBuilder.append(line);
+					}
+					inputStream.close();
+					reader.close();
+				} else
+				{
+					Log.d("JSON", "Failed to download file");
+				}
+			} catch (Exception e)
+			{
+				Log.d("readJSONFeed", e.getLocalizedMessage());
+			}
+			return stringBuilder.toString();
+		}
+
+		protected void onPostExecute(String result)
+		{
+			try
+			{
+				JSONObject jsonObject = new JSONObject(result);
+				System.out.println(jsonObject.getString("success"));
+				if (jsonObject.getString("success").toString().equals("1"))
+				{
+					
+					/*
+					// Success
+					JSONArray jsonProfileArray = (JSONArray) jsonObject
+							.getJSONArray("profile");
+
+					//String name = jsonProfileArray.getString(0) + " "
+						//	+ jsonProfileArray.getString(1);
+					String bio = jsonProfileArray.getString(3);
+					String img = jsonProfileArray.getString(5);
+
+					// decode image back to android bitmap format
+					byte[] decodedString = Base64.decode(img, Base64.DEFAULT);
+					if (decodedString != null)
+					{
+						bmp = BitmapFactory.decodeByteArray(decodedString, 0,
+								decodedString.length);
+					}
+					// set the image
+					if (bmp != null)
+					{
+						if (iv == null)
+						{
+							iv = (ImageView) findViewById(R.id.profilePhoto);
+
+						}
+						iv.setImageBitmap(bmp);
+						img = null;
+						decodedString = null;
+					}
+
+					TextView bioTextView = (TextView) findViewById(R.id.bioTextView);
+					bioTextView.setText(bio);
+					*/
+				} else
+				{
+					// Fail
+				}
+			} catch (Exception e)
+			{
+				Log.d("ReadatherJSONFeedTask", e.getLocalizedMessage());
+			}
+		}
+	}
 	
 	public void initKillswitchListener()
 	{
 		// START KILL SWITCH LISTENER
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction("CLOSE_ALL");
-		broadcastReceiver = new BroadcastReceiver()
-		{
-			@Override
-			public void onReceive(Context context, Intent intent)
-			{
-				// close activity
-				if (intent.getAction().equals("CLOSE_ALL"))
+				IntentFilter intentFilter = new IntentFilter();
+				intentFilter.addAction("CLOSE_ALL");
+				broadcastReceiver = new BroadcastReceiver()
 				{
-					Log.d("app666", "we killin the login it");
-					// System.exit(1);
-					finish();
-				}
-	
-			}
-		};
-		registerReceiver(broadcastReceiver, intentFilter);
-		// End Kill switch listener
+					@Override
+					public void onReceive(Context context, Intent intent)
+					{
+						// close activity
+						if (intent.getAction().equals("CLOSE_ALL"))
+						{
+							Log.d("app666", "we killin the login it");
+							// System.exit(1);
+							finish();
+						}
+
+					}
+				};
+				registerReceiver(broadcastReceiver, intentFilter);
+				// End Kill switch listener
 	}
 }
